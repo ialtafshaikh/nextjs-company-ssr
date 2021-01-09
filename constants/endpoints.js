dc7beb5e3f2759803ccb61662157461546be305a
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-09 12:10:18 +0530

    endpoint data uri modified name

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://ialtafshaikh.github.io/static-files/json/";
 
 export const contentstackDataURI = BASE_URL + "contentstack.json";
-export const rawengg = BASE_URL + "rawengg.json";
-export const surfboard = BASE_URL + "surfboard.json";
+export const rawenggDataURI = BASE_URL + "rawengg.json";
+export const surfboardDataURI = BASE_URL + "surfboard.json";
