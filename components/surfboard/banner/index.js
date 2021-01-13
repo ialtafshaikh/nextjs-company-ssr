3f2b0202e881f5cf6deb9007e3df460d512e2c2f
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-13 08:52:40 +0530

    surfboard banner implemented

--- components/surfboard/banner/index.js
+++ components/surfboard/banner/index.js
@@ -6,32 +6,12 @@ import bannerStyles from "./index.module.scss";
 export default function SurfboardBanner({ heroContent }) {
   return (
     <div style={{ height: "100vh" }} className="d-flex align-items-center">
-      <div className="container text-white d-flex justify-content-center">
-        <div className="text-center col-sm-12">
+      <div className="container text-dark d-flex justify-content-center">
+        <div className="text-left col-sm-12">
           <h1 className={bannerStyles["hero-heading"]}>
             {heroContent.heading}
           </h1>
           <p className={bannerStyles["hero-content"]}>{heroContent.content}</p>
-          <Button
-            className="px-5 my-5"
-            variant="primary"
-            style={{
-              backgroundColor: "#eb5646",
-              outline: "none",
-              border: "none",
-              letterSpacing: "1.1px",
-              fontWeight: "bold",
-            }}
-          >
-            Try For Free
-          </Button>
-          <p
-            style={{
-              color: "#eb5646",
-            }}
-          >
-            Request A Demo
-          </p>
         </div>
       </div>
     </div>
