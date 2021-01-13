23acddfd03a7b19f9a676373a949f660a91f7a70
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-13 09:35:32 +0530

    surfboard footer implemented

--- components/surfboard/footer/index.js
+++ components/surfboard/footer/index.js
@@ -6,129 +6,84 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Footer({ socialLinks, logo }) {
   return (
     <>
-      <footer>
+      <footer
+        style={{
+          borderTop: "#DFE1E5",
+          borderTopStyle: "solid",
+          borderTopWidth: "1px",
+        }}
+      >
         <div
           className="container mt-5"
-          style={{ paddingLeft: "0", paddingRight: "0" }}
+          style={{
+            paddingLeft: "0",
+            paddingRight: "0",
+          }}
         >
           <Row className="text-center">
             <Col md={3}>
               <img src={logo} alt="contentstack footer logo" width="160" />
-              <ul className="list-unstyled">
-                {Object.keys(socialLinks).map((key) => {
-                  return (
-                    <>
-                      <li className="list-inline-item">
-                        <a href={socialLinks[key]}>
-                          <FontAwesomeIcon
-                            icon={["fab", `${key}`]}
-                            className={footerStyle["socialLinks"]}
-                          />
-                        </a>
-                      </li>
-                    </>
-                  );
-                })}
-              </ul>
             </Col>
             <Col md={9}>
               <Row className="text-center">
                 <Col md={3} className="d-flex flex-column">
-                  <h2 className={footerStyle["footer-column-heading"]}>
-                    Product
-                  </h2>
                   <a className={footerStyle["footer-column-links"]} href="/#">
-                    Features
+                    Portfolio
                   </a>
                   <a className={footerStyle["footer-column-links"]} href="/#">
-                    For IT {`&`} Developers
+                    Academy
                   </a>
                   <a className={footerStyle["footer-column-links"]} href="/#">
-                    For Business
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    Integrations
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    Pricing
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    ROI Calculator
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    Customer Success
+                    Events
                   </a>
                 </Col>
                 <Col md={3} className="d-flex flex-column">
-                  <h2 className={footerStyle["footer-column-heading"]}>
-                    Users
-                  </h2>
                   <a className={footerStyle["footer-column-links"]} href="/#">
-                    Supported Platforms
+                    Gallery
                   </a>
                   <a className={footerStyle["footer-column-links"]} href="/#">
-                    FAQs
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    Trust
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    System Status
-                  </a>
-                </Col>
-                <Col md={3} className="d-flex flex-column">
-                  <h2 className={footerStyle["footer-column-heading"]}>
-                    Education
-                  </h2>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    Customer Resources
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    CMS Guides
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    Blog
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    Docs
-                  </a>
-                </Col>
-                <Col md={3} className="d-flex flex-column">
-                  <h2 className={footerStyle["footer-column-heading"]}>
-                    Company
-                  </h2>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    About
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    News
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    Press
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    Careers
-                  </a>
-                  <a className={footerStyle["footer-column-links"]} href="/#">
-                    Become a Partner
+                    e-certificates
                   </a>
                   <a className={footerStyle["footer-column-links"]} href="/#">
                     Contact Us
                   </a>
                 </Col>
+                <Col md={3} className="d-flex flex-column">
+                  <a
+                    className={footerStyle["footer-column-links"]}
+                    href="mailto:info@surfboardventures.com?subject=Enquiry"
+                    target="_blank"
+                  >
+                    info@surfboardventures.com
+                  </a>
+                </Col>
+                <Col md={3} className="d-flex flex-column">
+                  <ul className="list-unstyled">
+                    {Object.keys(socialLinks).map((key) => {
+                      return (
+                        <>
+                          <li className="list-inline-item">
+                            <a href={socialLinks[key]}>
+                              <FontAwesomeIcon
+                                icon={["fab", `${key}`]}
+                                className={footerStyle["socialLinks"]}
+                              />
+                            </a>
+                          </li>
+                        </>
+                      );
+                    })}
+                  </ul>
+                </Col>
               </Row>
             </Col>
           </Row>
-          <hr
-            className="font-weight-bold my-5"
-            style={{ background: "black", width: "95%" }}
-          />
-          <Row className="text-right px-3">
+          <hr className="mt-5" />
+          <Row>
             <Col>
-              <ul className="list-unstyled list-inline pb-5">
-                <li className="list-inline-item">Terms</li>
-                <li className="list-inline-item">Privacy</li>
-              </ul>
+              <p className={footerStyle["footer-copyright"]}>
+                Copyright © 2020 Surfboard Ventures. All Rights Reserved.
+              </p>
             </Col>
           </Row>
         </div>
