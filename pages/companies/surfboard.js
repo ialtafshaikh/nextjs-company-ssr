f96121b81979b302e0ff65a955015d94606c1842
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-13 08:17:03 +0530

    surfboard page structured

--- pages/companies/surfboard.js
+++ pages/companies/surfboard.js
@@ -1,7 +1,45 @@
+import axios from "axios";
+
+// components
+import Layout from "../../components/Layout";
+import Footer from "../../components/Footer/";
+
+// constants
+import { surfboardDataURI } from "../../constants/endpoints";
+
 export default function surfboard() {
+  const headerStyle = {
+    backgroundImage: `url(${props.companyData.bannerImage})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+  };
   return (
     <div>
+      <Layout
+        title="Contentstack | Home"
+        Footer={Footer}
+        favicon={props.companyData.favicon}
+        footerContent={{
+          socialLinks: props.companyData.socialLinks,
+          logo: props.companyData.companyLogo,
+        }}
+      >
+        <header style={headerStyle}>
+          <Navbar logo={props.companyData.companyLogo} />
+          <Banner heroContent={props.companyData.heroContent} />
+        </header>
+      </Layout>
       <h1>Surfboard</h1>
     </div>
   );
 }
+
+export const getStaticProps = async (context) => {
+  let { data } = await axios(surfboardDataURI);
+
+  return {
+    props: { companyData: { ...data[0] } },
+  };
+};
