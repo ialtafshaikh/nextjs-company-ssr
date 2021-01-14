08c01014fad580b1b8d10f79a8eb8f09ab57c29d
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-14 11:25:40 +0530

    home data uri added in endpoint

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -3,3 +3,4 @@ const BASE_URL = "https://ialtafshaikh.github.io/static-files/json/";
 export const contentstackDataURI = BASE_URL + "contentstack.json";
 export const rawenggDataURI = BASE_URL + "rawengg.json";
 export const surfboardDataURI = BASE_URL + "surfboard.json";
+export const homeDataURI = BASE_URL + "home.json";
