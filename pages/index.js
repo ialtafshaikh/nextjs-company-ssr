a6ddaddec9ea14773f76a52a01af3d7d2ffb8f9f
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-14 11:17:49 +0530

    home page with company logo implemented

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,36 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" type="image/png" sizes="32x32" href="vercel.svg" />
       </Head>
-      <main></main>
+      <main>
+        <div style={{ height: "100vh" }} className="d-flex align-items-center">
+          <div className="container text-white d-flex justify-content-center">
+            <div className="col-sm text-center d-flex-column w-100">
+              <a href="/companies/surfboard">
+                <img
+                  src="https://www.surfboardventures.com/favicon.ico"
+                  width="70"
+                  height="70"
+                />
+              </a>
+              <a href="/companies/contentstack">
+                {" "}
+                <img
+                  className={styles["image-style"]}
+                  src="https://www.contentstack.com/favicon.ico"
+                />
+              </a>
+              <a href="/companies/rawengg">
+                <img
+                  className=""
+                  src="https://www.raweng.com/icons/icon-144x144.png"
+                  width="70"
+                  height="70"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
       <style jsx>
         {`
           main {
