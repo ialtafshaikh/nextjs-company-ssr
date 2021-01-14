3c9d323660f00e1cb0db10de169d78a04d1cc394
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-14 16:10:52 +0530

    console warnings fixed

--- components/contentstack/footer/index.js
+++ components/contentstack/footer/index.js
@@ -21,10 +21,10 @@ export default function Footer({ socialLinks, logo }) {
                 height={30}
               />
               <ul className="list-unstyled">
-                {Object.keys(socialLinks).map((key) => {
+                {Object.keys(socialLinks).map((key, index) => {
                   return (
                     <>
-                      <li className="list-inline-item">
+                      <li className="list-inline-item" key={index}>
                         <a href={socialLinks[key]}>
                           <FontAwesomeIcon
                             icon={["fab", `${key}`]}
